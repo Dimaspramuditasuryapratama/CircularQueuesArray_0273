bd1780133ee74515dc55fec6b64e0ddfdfc245f8
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-06-13 08:24:59 +0700

    remove method in Queues class to properly handle single element deletion

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -46,6 +46,16 @@ public:
         }
         cout << "\nThe element deleted from the queues is: " << queue_array[FRONT] << endl;
 
-    
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        } else {
+            // jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1) 
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
     }
 };
